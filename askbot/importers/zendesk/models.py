f83c717f2826dd88e0888720cbcd4d6d11733530
KP <kp@shotgunsoftware.com>
2013-05-22 12:41:03 -0500

    defaults for zendesk_entry table

--- askbot/importers/zendesk/models.py
+++ askbot/importers/zendesk/models.py
@@ -12,10 +12,10 @@ class Entry(models.Model):
     """
     body = models.TextField()
     created_at = models.DateTimeField()
-    tags = models.CharField(max_length = 255)
+    tags = models.CharField(max_length = 255, null = True)
     flag_type_id = models.IntegerField() # topic type
     forum_id = models.IntegerField() # forum entry is in
-    hits = models.IntegerField() # number of views
+    hits = models.IntegerField(default = 0) # number of views
     entry_id = models.IntegerField()
     is_highlighted = models.BooleanField(default = False) # ignored
     is_locked = models.BooleanField(default = False) # close
@@ -23,12 +23,12 @@ class Entry(models.Model):
     is_public = models.BooleanField(default = True)
     organization_id = models.IntegerField()
     position = models.IntegerField() # ignored
-    posts_count = models.IntegerField()
+    posts_count = models.IntegerField(default = 0)
     submitter_id = models.IntegerField()
     title = models.CharField(max_length = 300)
     updated_at = models.DateTimeField()
-    votes_count = models.IntegerField()
-    ab_id = models.IntegerField()
+    votes_count = models.IntegerField(default = 0)
+    ab_id = models.IntegerField(null = True)
 
     def get_author(self):
         """returns author of the post, from the Django user table"""
